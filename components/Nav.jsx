adc55f082eec929e22508e43201b605cd6035eed
GraceYourCode <freshmannskuls@gmail.com>
2024-04-10 14:17:42 -0700

    first commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -29,6 +29,9 @@ const Nav = () => {
 
           <button type="button" onClick={signOut}
             className="text-black bg-white border border-black rounded-3xl py-1.5 px-7 text-sm">Sign Out</button>
+
+          <img src="src" alt={session?.user.image}
+          className="w-16 h-16 rounded-full" />
         </div>
       ) : (
         <>
