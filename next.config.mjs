982e6ef7691c820f9f4c754eeae9b1a2d1ed23af
GraceYourCode <freshmannskuls@gmail.com>
2024-04-10 19:24:41 -0700

    more commits for functionalities

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   appDocumentPreloadingr: true,
-  //   serverComponentsExternalPackages: ["mongoose"],
-  // },
-  // images: {
-  //   domains: ["lh3.googleusercontent.com"],
-  // },
-  // webpack(config) {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true,
-  //   }
-  //   return config
-  // }
+  
 };
 
 export default nextConfig;
