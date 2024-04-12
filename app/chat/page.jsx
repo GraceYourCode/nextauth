d0506912dbfa9a1f16a0c492f3b136be39d0e47f
GraceYourCode <freshmannskuls@gmail.com>
2024-04-12 15:11:02 -0700

    adding the real time functionalities

--- app/chat/page.jsx
+++ app/chat/page.jsx
@@ -1,10 +1,36 @@
-import Chat from "@/pages/chat"
+"use client"
+
+import Comments from "@/components/Comments";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+
 
 const ChatPage = () => {
 
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState();
+
+  async function getStaticProps() {
+    // Fetch your data (e.g., blog posts) from an API or database
+    const posts = await fetch('/api/comments/all').then((res) => res.json());
+
+    setPosts(posts)
+  }
+
+  //to get posts from the server
+  setInterval(() => {
+    getStaticProps();
+  }, 5000);
+
   return (
     <>
-      <Chat />
+      <div className="flex justify-center w-screen">
+        <main className=" w-1/2 flex flex-col gap-y-4 items-center py-5">
+          <Comments posts={posts} />
+          {session?.user && <Textbox />}
+        </main>
+      </div>
     </>
   )
 }
