e33b92ed718a592b83ab15d79f7c0e95dc682172
GraceYourCode <freshmannskuls@gmail.com>
2024-04-12 01:06:18 -0700

    more functionalities

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
-  const [allComments, setAllComments] = useState();
+  const [allComments, setAllComments] = useState(undefined);
 
   useEffect(() => {
     const fectchComments = async () => {
@@ -20,40 +20,40 @@ export default function Home() {
     fectchComments();
   }, []);
 
-  const getTimeDifference = (recordedDateString) => {
-    // Convert the recorded date string to a Date object
-    const recordedDate = new Date(recordedDateString);
+//   const getTimeDifference = (recordedDateString) => {
+//     // Convert the recorded date string to a Date object
+//     const recordedDate = new Date(recordedDateString);
 
-    // Get the current date and time
-    const currentDate = new Date();
+//     // Get the current date and time
+//     const currentDate = new Date();
 
-    // Calculate the time difference in milliseconds
-    const timeDifference = currentDate.getTime() - recordedDate.getTime();
+//     // Calculate the time difference in milliseconds
+//     const timeDifference = currentDate.getTime() - recordedDate.getTime();
 
-    // Convert milliseconds to hours, minutes, and seconds
-    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+//     // Convert milliseconds to hours, minutes, and seconds
+//     const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+//     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    // Return the formatted time difference
-    return `${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
-}
+//     // Return the formatted time difference
+//     return `${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
+// }
 
   return (
     <div className="flex justify-center w-screen">
-      <main className=" w-1/2 flex flex-col gap-y-4 items-center py-5">
+      {/* <main className=" w-1/2 flex flex-col gap-y-4 items-center py-5">
         {allComments &&
           allComments.map(comment => (
             <Comment 
             content={comment.content}
-            dateCreated={getTimeDifference(comment.dateCreated)}
+            dateCreated={comment.dateCreated}
             likes={comment.likes}
             username={comment.creator.username}
             key={comment._id} />
           ))
         }
         {session?.user && <Textbox />}
-      </main>
+      </main> */}
     </div>
   );
 }
