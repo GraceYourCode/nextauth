4cdafe5949539ac5f4a7467a0cbd38d57d70c95f
GraceYourCode <freshmannskuls@gmail.com>
2024-04-12 03:11:11 -0700

    more functionalities

--- app/api/comments/all/route.js
+++ app/api/comments/all/route.js
@@ -5,38 +5,16 @@ export const GET = async () => {
   try {
     const dataBase = await connectToDB();
 
-    //     const db = await connectToMongoDB();
-    // const collection = db.collection('your-collection-name');
-    // const data = await collection.find({}).toArray();
-    const collection = new dataBase.Collection("comments").find({}).toArray();
-    // const data = await collection
-console.log(data, "i wan see wetin dey sup")
-    // const comments = await Comment.find({}).populate("creator");
-    const comments = [
-      {
-        "serious": "more serious",
-        "abi": "kila ma tun se",
-        "works?": "if this doesn't work?..."
-      },
-      {
-        "serious": "more serious",
-        "abi": "kila ma tun se",
-        "works?": "if this doesn't work?..."
-      },
-      {
-        "serious": "more serious",
-        "abi": "kila ma tun se",
-        "works?": "if this doesn't work?..."
-      },
-    ]
+    const collection = new dataBase.Collection("comments", dataBase.connection, {});
+    const data = await collection.find({}).toArray();
+    console.log(data, "i wan see wetin dey sup")
 
-    console.log(comments)
-    console.log(collection)
 
-    return new Response(JSON.stringify(comments), {
+    return new Response(JSON.stringify(data), {
       status: 200,
     })
   } catch (error) {
+    console.log(error)
     return new Response(JSON.stringify("Failed to fetch comments"), {
       status: 500,
     })
--- app/page.js
+++ app/page.js
@@ -20,40 +20,42 @@ export default function Home() {
     fectchComments();
   }, []);
 
-//   const getTimeDifference = (recordedDateString) => {
-//     // Convert the recorded date string to a Date object
-//     const recordedDate = new Date(recordedDateString);
+  const getTimeDifference = (recordedDateString) => {
+    // Convert the recorded date string to a Date object
+    const recordedDate = new Date(recordedDateString);
 
-//     // Get the current date and time
-//     const currentDate = new Date();
+    // Get the current date and time
+    const currentDate = new Date();
 
-//     // Calculate the time difference in milliseconds
-//     const timeDifference = currentDate.getTime() - recordedDate.getTime();
+    // Calculate the time difference in milliseconds
+    const timeDifference = currentDate.getTime() - recordedDate.getTime();
 
-//     // Convert milliseconds to hours, minutes, and seconds
-//     const hours = Math.floor(timeDifference / (1000 * 60 * 60));
-//     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-//     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    // Convert milliseconds to hours, minutes, and seconds
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-//     // Return the formatted time difference
-//     return `${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
-// }
+    // Return the formatted time difference
+    if (hours > 0) return `${hours} ${hours < 2 ? "hour" : "hours"} ago`;
+    else if (minutes > 0) return `${minutes} ${minutes < 2 ? "minute" : "minutes"} ago`;
+    else return `${seconds} ${seconds < 2 ? "second": "seconds"} ago`;
+}
 
   return (
     <div className="flex justify-center w-screen">
-      {/* <main className=" w-1/2 flex flex-col gap-y-4 items-center py-5">
+      <main className=" w-1/2 flex flex-col gap-y-4 items-center py-5">
         {allComments &&
           allComments.map(comment => (
             <Comment 
             content={comment.content}
-            dateCreated={comment.dateCreated}
+            dateCreated={getTimeDifference(comment.dateCreated)}
             likes={comment.likes}
             username={comment.creator.username}
             key={comment._id} />
           ))
         }
         {session?.user && <Textbox />}
-      </main> */}
+      </main>
     </div>
   );
 }
