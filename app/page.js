888b7fbe47305341e7a205eae323ee7bb2a41200
GraceYourCode <freshmannskuls@gmail.com>
2024-04-11 21:29:21 -0700

    more functionalities

--- app/page.js
+++ app/page.js
@@ -1,12 +1,26 @@
+"use client"
+
 import Comment from "@/components/Comment";
 import Textbox from "@/components/Textbox";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const [allComments, setAllComments] = useState();
+
+  useEffect(() => {
+    fetch("/api/comments/all")
+      .then(res=> res.json)
+      .then(data=> console.log(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className="flex justify-center w-screen">
       <main className=" w-1/2 flex items-center py-5">
         <Comment />
-        <Textbox />
+        {session?.user && <Textbox />}
       </main>
     </div>
   );
