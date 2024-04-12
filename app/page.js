c7836d280ee5f12b6ad3917c23d5cb5130611755
GraceYourCode <freshmannskuls@gmail.com>
2024-04-12 13:38:25 -0700

    more functionalities

--- app/page.js
+++ app/page.js
@@ -5,18 +5,33 @@ import Textbox from "@/components/Textbox";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
-export default function Home() {
+
+export default function Home({ allComm }) {
   const { data: session } = useSession();
   const [allComments, setAllComments] = useState();
 
   useEffect(() => {
-    const fectchComments = async () => {
-      const response = await fetch("/api/comments/all");
-      const data = await response.json();
-      setAllComments(data);
+    const getStaticProps = async () => {
+      // Fetch your data (e.g., blog posts) from an API or database
+      const posts = await fetch('/api/comments/all', {
+        next: {
+          revalidate: 10,
+        },
+      })
+        .then(res => res.json());
+
+      console.log(posts);
+      setAllComments(posts);
+
+      // return {
+      //   props: {
+      //     posts,
+      //   },
+      //   revalidate: 10, // Revalidate every 10 seconds
+      // };
     }
 
-    fectchComments();
+    getStaticProps();
   }, []);
 
   const getTimeDifference = (recordedDateString) => {
@@ -37,20 +52,20 @@ export default function Home() {
     // Return the formatted time difference
     if (hours > 0) return `${hours} ${hours < 2 ? "hour" : "hours"} ago`;
     else if (minutes > 0) return `${minutes} ${minutes < 2 ? "minute" : "minutes"} ago`;
-    else return `${seconds} ${seconds < 2 ? "second": "seconds"} ago`;
-}
+    else return `${seconds} ${seconds < 2 ? "second" : "seconds"} ago`;
+  }
 
   return (
     <div className="flex justify-center w-screen">
       <main className=" w-1/2 flex flex-col gap-y-4 items-center py-5">
         {allComments &&
           allComments.map(comment => (
-            <Comment 
-            content={comment.content}
-            dateCreated={getTimeDifference(comment.dateCreated)}
-            likes={comment.likes}
-            username={comment.creator.username}
-            key={comment._id} />
+            <Comment
+              content={comment.content}
+              dateCreated={getTimeDifference(comment.dateCreated)}
+              likes={comment.likes}
+              username={comment.creator.username}
+              key={comment._id} />
           ))
         }
         {session?.user && <Textbox />}
