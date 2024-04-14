4e7fe9fea48c20fee028610f50c4d2db080f006a
GraceYourCode <freshmannskuls@gmail.com>
2024-04-13 23:21:47 -0700

    fixed the bugs

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,8 +3,6 @@
 import { getProviders, signIn, signOut, useSession } from "next-auth/react"
 
 import { useEffect, useState } from "react";
-import Image from "next/image"
-
 
 
 const Navigation = () => {
@@ -13,40 +11,47 @@ const Navigation = () => {
   const [providers, setProviders] = useState(null);
   const [fixed, setFixed] = useState(false)
 
-
-  // keep track of previous scroll position
-  let prevScrollPos = window.scrollY;
-
-  const onScroll = () => {
-    // current scroll position
-    const currentScrollPos = this.window.scrollY;
-
-    if (prevScrollPos > currentScrollPos) {
-      // user has scrolled up
-      if (currentScrollPos === 0) setFixed(false);
-      else setFixed(true);
-    } else setFixed(false);
-
-    // update previous scroll position
-    prevScrollPos = currentScrollPos;
-  }
-
-  
   useEffect(() => {
     const setToProviders = async () => {
       const response = await getProviders();
-      
-      setProviders(response)
-      
-    }
-    
-    setToProviders();
-    
-    window.addEventListener('scroll', onScroll);
 
-    return () => {
-      window.removeEventListener("scroll", onScroll)
+      setProviders(response)
+
     }
+
+    setToProviders();
+
+    if (typeof window !== 'undefined') {
+      // Code that uses window (only runs in the browser)
+
+      // keep track of previous scroll position
+      let prevScrollPos = window.scrollY;
+
+      const onScroll = () => {
+        // current scroll position
+        const currentScrollPos = window.scrollY;
+
+        if (prevScrollPos > currentScrollPos) {
+          // user has scrolled up
+          if (currentScrollPos === 0) setFixed(false);
+          else setFixed(true);
+        } else setFixed(false);
+
+        // update previous scroll position
+        prevScrollPos = currentScrollPos;
+      }
+
+      window.addEventListener('scroll', onScroll);
+
+      return () => {
+        window.removeEventListener("scroll", onScroll)
+      }
+
+    }
+
+
+
+
 
   }, [])
 
--- components/Textbox.jsx
+++ components/Textbox.jsx
@@ -7,7 +7,7 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { IoIosSend } from "react-icons/io";
-import posts from "@/store/store";
+
 
 const Textbox = () => {
   const socket = io("http://localhost:4000");;
@@ -19,33 +19,39 @@ const Textbox = () => {
   const [fixed, setFixed] = useState(true);
   const router = useRouter();
 
-  // keep track of previous scroll position
-  let prevScrollPos = window.scrollY;
-
-  const onScroll = () => {
-    // current scroll position
-    const currentScrollPos = window.scrollY;
-
-    // if the user scrolls up
-    if (prevScrollPos > currentScrollPos) setFixed(true);
-    else {
-      // get the height of the window plus the scroll position
-      const scrolledTo = window.scrollY + window.innerHeight;
-
-      // change the text box from fixed and scroll to bottom of page
-      if (document.body.scrollHeight === scrolledTo) setFixed(false);
-    }
-
-    // updates the value of scroll position
-    prevScrollPos = currentScrollPos;
-  }
 
   useEffect(() => {
-    window.addEventListener('scroll',onScroll)
 
-    return () => {
-      window.removeEventListener("scroll", onScroll)
+    if (typeof window !== 'undefined') {
+      // Code that uses window (only runs in the browser)
+      let prevScrollPos = window.scrollY;
+
+      const onScroll = () => {
+        // current scroll position
+        const currentScrollPos = window.scrollY;
+
+        // if the user scrolls up
+        if (prevScrollPos > currentScrollPos) setFixed(true);
+        else {
+          // get the height of the window plus the scroll position
+          const scrolledTo = window.scrollY + window.innerHeight;
+
+          // change the text box from fixed and scroll to bottom of page
+          if (document.body.scrollHeight === scrolledTo) setFixed(false);
+        }
+
+        // updates the value of scroll position
+        prevScrollPos = currentScrollPos;
+      }
+
+      window.addEventListener('scroll', onScroll)
+
+      return () => {
+        window.removeEventListener("scroll", onScroll)
+      }
+
     }
+    // keep track of previous scroll position
   }, [])
 
 
