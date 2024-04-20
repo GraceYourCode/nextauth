616b21051d1b9633aa66d3eaf62a20305f234511
GraceYourCode <freshmannskuls@gmail.com>
2024-04-19 17:39:30 -0700

    custom delete and edit

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,10 +1,14 @@
 import { FaReply } from "react-icons/fa";
+import { MdOutlineEdit, MdDelete } from "react-icons/md";
 
 const Button = ({ hide, click, id, type }) => {
   return (
-    <button className={`text-blue ${hide && "hidden sm:flex"} flex gap-1 items-center group text-sm font-medium`} onClick={()=>click(id)}>
-      <FaReply className="group-hover:text-gray-blue" />
-      <span className="group-hover:text-gray-blue">{type}</span>
+    <button className={`${type === "Delete" ? "text-red" : "text-blue"} ${hide && "hidden sm:flex"} flex gap-1 items-center group text-sm font-medium`} onClick={() => click(id)}>
+      {
+        type === "Reply" ? <FaReply className="group-hover:text-gray-blue" /> : 
+          type === "Edit" ? <MdOutlineEdit className="group-hover:text-gray-blue" /> : <MdDelete className="group-hover:text-pale-red" />
+        }
+        <span className="group-hover:text-gray-blue">{type}</span>
     </button>
   )
 }
