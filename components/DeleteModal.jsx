a10cee120c548e2d8cf0fa05e53c104a16bd2aa9
GraceYourCode <freshmannskuls@gmail.com>
2024-04-21 22:03:46 -0700

    tests

--- components/DeleteModal.jsx
+++ components/DeleteModal.jsx
@@ -8,12 +8,13 @@ const DeleteModal = () => {
   const { toDelete } = useContext(posts);
 
   const deleteComment = async () => {
-    console.log(toDelete)
-    const response = await fetch(`api/comments/${toDelete}`, {
-      method: "DELETE",
-    })
-    const data = await response.json();
-    console.log(data);
+    console.log(toDelete);
+
+    // const response = await fetch(`api/comments/${toDelete}`, {
+    //   method: "DELETE",
+    // })
+    // const data = await response.json();
+    // console.log(data);
   }
   return (
     <>
